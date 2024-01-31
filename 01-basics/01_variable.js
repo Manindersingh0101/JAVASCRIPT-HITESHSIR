aa5865768970acbb88b144cd474307e2fdd1a8a7
Manindersingh0101 <116154370+Manindersingh0101@users.noreply.github.com>
2024-01-31 13:50:30 +0000

    datatype

---
+++ 01-basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountID = 14443
+let accountEmail = "maninder@google.com"
+var accountpasswood = "12345"
+accountcity = "jaipur"
+
+//accountID = 2// not allowed
+
+accountEmail = "havy@.com"
+accountpasswood = "324234323443"
+accountcity = "amritsar"
+let accountstate;
+console.log(accountID);
+/*
+prefer not to use var
+
+
+
+
+
+
+*/
+console.table([accountID,accountEmail,accountcity,accountpasswood,accountstate])
