b09ccc7eb07a86a1919f4c9ccd97157db8c28fe5
Manindersingh0101 <116154370+Manindersingh0101@users.noreply.github.com>
2024-01-31 13:50:05 +0000

    datatype

---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(  3+3 ) we are using nodejs, not browser
+
+console.log( 3 + 3);
+
+// make a code readability shoud be high
+
+
+let name = "maninder"
+let age = 18
+let isloggedIn = false
+let state = null
+
+//number => 2 tp power 53
+//bigint 
+// string => ""
+//bollean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+//preemtitive datatyoe
+
+
+
+
+//obj
+console.log(typeof null);//  object
