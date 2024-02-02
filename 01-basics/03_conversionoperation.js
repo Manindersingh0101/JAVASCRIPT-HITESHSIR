a947d60e76a47b29c553dd7d30aa0ecd7199a931
Manindersingh0101 <116154370+Manindersingh0101@users.noreply.github.com>
2024-02-02 12:45:47 +0000

    Why string to number conversion is confusing

--- 01-basics/03_conversionoperation.js
+++ 01-basics/03_conversionoperation.js
@@ -1,9 +1,9 @@
-let score = "hitesh"
+// let score = "hitesh"
 
 //console.log(typeof score);
 //console.log(typeof(score));
 
-let ValueInNumber = Number(score)
+// let ValueInNumber = Number(score)
 //console.log(typeof ValueInNumber);
 //console.log(ValueInNumber);
 
@@ -11,18 +11,57 @@ let ValueInNumber = Number(score)
 // "33abc" => NaN
 //  True => 1; false => 0;
 
-let isLoggedIn = "maninder"
+// let isLoggedIn = "maninder"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "mainder" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let StringNumber = String(someNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
 
+//***************************Operations **************************** */
+
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let strl1 = "hello"
+let strl2 =" maninder"
+  let strl3 = strl1 + strl2
+  // console.log(strl3);
+
+  // console.log("1" + 2);
+  // console.log(1 + "2");
+  // console.log("1" + 2 + 2);
+  // console.log(1 + 2 + "2");
+
+
+  // console.log(+true);
+  // console.log(+"");
+let num1, num2, num3
+
+num1 = num2 =num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter1 = 100
+++gameCounter1;
+console.log(gameCounter1);
