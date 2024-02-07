be3e7b6434532ff749057c83150ea0be00f58fe4
Manindersingh0101 <116154370+Manindersingh0101@users.noreply.github.com>
2024-02-07 16:16:29 +0000

    conversion

--- 01-basics/03_conversionoperation.js
+++ 01-basics/03_conversionoperation.js
@@ -1,11 +1,19 @@
+//let score = 33
+//let score1="33"
+//console.log(typeof score); // output is number
+//console.log(typeof (score1));// output is string
+
+//javascipt is case sentitive language
+
 // let score = "hitesh"
 
 //console.log(typeof score);
 //console.log(typeof(score));
 
 // let ValueInNumber = Number(score)
-//console.log(typeof ValueInNumber);
-//console.log(ValueInNumber);
+//console.log(typeof ValueInNumber);// output number
+//console.log(ValueInNumber); ////    output NaN
+
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,54 +22,67 @@
 // let isLoggedIn = "maninder"
 
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn); // output true
+
+//let isLoggedIn = ""
+
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn); // output false
+
 
 // 1 => true; 0 => false
 // "" => false
 // "mainder" => true
 
-// let someNumber = 33
 
-// let StringNumber = String(someNumber)
-// console.log(StringNumber);
-// console.log(typeof StringNumber);
+ //let someNumber = 33
+
+ //let stringNumber = String(someNumber)
+//console.log(stringNumber);  // output 33
+//console.log(typeof stringNumber); //output string
 
 //***************************Operations **************************** */
 
-let value = 3 
-let negValue = -value
-//console.log(negValue);
+//let value = 3 
+//let negValue = -value
+//console.log(negValue); // output -3
 
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
 
-// console.log(2**3);
+// console.log(2**3); // eg 2 de power 3 = 8
 // console.log(2/3);
-// console.log(2%3);
+// console.log(2%3); // reminder
 
 
-let strl1 = "hello"
-let strl2 =" maninder"
-  let strl3 = strl1 + strl2
-  // console.log(strl3);
+//let str1 = "hello"
+//let str2 =" maninder"
+  //let str3 = strl1 + strl2
+  // console.log(strl3);  ////  output hello maninder
 
-  // console.log("1" + 2);
-  // console.log(1 + "2");
-  // console.log("1" + 2 + 2);
-  // console.log(1 + 2 + "2");
+  // console.log("1" + 2);////  output  12
+  // console.log(1 + "2");////  output   12 
+  // console.log("1" + 2 + 2);////  output   122
+  // console.log(1 + 2 + "2");////  output   32
 
 
-  // console.log(+true);
-  // console.log(+"");
-let num1, num2, num3
+  // console.log(true);////  output  true
+  // console.log(+true);////  output 1
+  // console.log(+"");////  output  0 
 
-num1 = num2 =num3 = 2 + 2
 
-let gameCounter = 100
-gameCounter++;
-console.log(gameCounter);
+  
+//let num1, num2, num3  // ex
 
-let gameCounter1 = 100
-++gameCounter1;
-console.log(gameCounter1);
+//num1 = num2 =num3 = 2 + 2   //ex
+
+//let gameCounter = 100
+//gameCounter++;
+//console.log(gameCounter);  ///// output 101 
+
+//let gameCounter1 = 100
+//++gameCounter1;
+//console.log(gameCounter1); ///// output 101 
+
