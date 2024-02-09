f2601bc97229859c4d0110a198aa0717494557b8
Manindersingh0101 <116154370+Manindersingh0101@users.noreply.github.com>
2024-02-09 14:27:32 +0000

    COMPARISION

--- 01-basics/04_comparision.js
+++ 01-basics/04_comparision.js
@@ -1,22 +1,33 @@
-// console.log( 2 > 1);
-// console.log( 2 >= 1);
-// console.log( 2 < 1);
-// console.log( 2 == 1);
-// console.log( 2 != 1);
+// console.log( 2 > 1);//// output
+// console.log( 2 >= 1);//// output
+// console.log( 2 < 1);//// output
+// console.log( 2 == 1);//// output
+// console.log( 2 != 1);//// output
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);//// output
+// console.log("02" > 1);//// output
 
 // avoid thiss type of comparision conversion 
 // code shoud be clean
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);  //// output   false
+console.log(null == 0);//// output     
+console.log(null >= 0);//// output
+/**The reason is that an equality check == and comparisons > < >= <= work differently. Comparisons convert null to a number, treating it as 0. That’s why (3) null >= 0 is true and (1) null > 0 is false.
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+On the other hand, the equality check == for undefined and null is defined such that, without any conversions, they equal each other and don’t equal anything else. That’s why (2) null == 0 is false.
 
-// ===
+null is treated in a special way by the Equals Operator (==). In a brief, it only coerces to undefined:
 
-console.log("2" === 2);
+null == null; // true
+null == undefined; // true
+Value such as false, '', '0', and [] are subject to numeric type coercion, all of them coerce to zero.
+
+ */
+
+// console.log(undefined == 0);//// output
+// console.log(undefined > 0);//// output
+// console.log(undefined < 0);//// output
+
+// // ===
+
+// console.log("2" === 2);//// output
