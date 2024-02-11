1278dab21548f1f7ec46a8db7c0a807ba3081ed4
Manindersingh0101 <116154370+Manindersingh0101@users.noreply.github.com>
2024-02-11 14:28:54 +0000

    give comparisionmore eg

--- 01-basics/04_comparision.js
+++ 01-basics/04_comparision.js
@@ -1,17 +1,23 @@
-// console.log( 2 > 1);//// output
-// console.log( 2 >= 1);//// output
-// console.log( 2 < 1);//// output
-// console.log( 2 == 1);//// output
-// console.log( 2 != 1);//// output
+// console.log( 2 > 1);   //// output   true
+// console.log( 2 >= 1);   //// output   true
+// console.log( 2 < 1);    //// output   false
+// console.log( 2 == 1);    //// output  false
+// console.log( 2 != 1); //// output   true
 
-// console.log("2" > 1);//// output
-// console.log("02" > 1);//// output
+// console.log("2" > 1);//// output   true
+// console.log("02" > 1);//// output  true
 
 // avoid thiss type of comparision conversion 
 // code shoud be clean
-console.log(null > 0);  //// output   false
-console.log(null == 0);//// output     
-console.log(null >= 0);//// output
+
+// console.log(null > 0);     //// output   false
+// console.log(null == 0);    //// output     false
+// console.log(null >= 0);    //// output       true
+
+// console.log(undefined == 0);//// output
+// console.log(undefined > 0);//// output
+// console.log(undefined < 0);//// output
+
 /**The reason is that an equality check == and comparisons > < >= <= work differently. Comparisons convert null to a number, treating it as 0. That’s why (3) null >= 0 is true and (1) null > 0 is false.
 
 On the other hand, the equality check == for undefined and null is defined such that, without any conversions, they equal each other and don’t equal anything else. That’s why (2) null == 0 is false.
@@ -24,10 +30,8 @@ Value such as false, '', '0', and [] are subject to numeric type coercion, all o
 
  */
 
-// console.log(undefined == 0);//// output
-// console.log(undefined > 0);//// output
-// console.log(undefined < 0);//// output
+
 
 // // ===
 
-// console.log("2" === 2);//// output
+ console.log("2" === 2);//// output  false
